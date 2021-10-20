3e608a7e6bc4f7f3040ee1a94ba96360c77a1204
asardon <aetienne.sardon@gmail.com>
2021-10-20 19:36:03 +0200

    fixed comment

--- src/migrations/2_deploy_contracts.js
+++ src/migrations/2_deploy_contracts.js
@@ -29,8 +29,8 @@ module.exports = function (deployer) {
     deploymentConfig.settlement_duration, // set to 100 to be able to use time.advanceBlockTo //57600+172800+5760 -> approx. 1 day after amm_end
     deploymentConfig.collateral_ccy, // "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2" -> WETH (mainnet)
     deploymentConfig.borrow_ccy, // "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48" -> USDC (mainnet)
-    deploymentConfig.collateral_ccy_eq_factor, // e.g., for 1 WETH per 2000 USDC -> "2" because: 10^18 <=> 2000*10^6, => 2*10^18 = 2*10^12*10^6
-    deploymentConfig.borrow_ccy_eq_factor, // e.g., for 1 WETH per 2000 USDC -> "2*10^12" because: 10^18 <=> 2000*10^6, => 2*10^18 = 2*10^12*10^6
+    deploymentConfig.collateral_ccy_eq_factor, // e.g., for 1 WETH per 2000 USDC -> "2000" because: 1ETH(10^18) <=> 2000USDC(10^6), => 2000*(1*10^18) = 10^12*(2000*10^6)
+    deploymentConfig.borrow_ccy_eq_factor, // e.g., for 1 WETH per 2000 USDC -> "10^12" because: 1ETH(10^18) <=> 2000USDC(10^6), => 2000*(1*10^18) = 10^12*(2000*10^6)
     deploymentConfig.alpha, // 200000000 -> 0.2, i.e., 50% of ATM BS approx, hence 0.5*0.4 = 0.2
     deploymentConfig.init_collateral_price, // 3000*1000000 (scaled according to borrow cccy ERC20 decimals, USDC=10^6)
     deploymentConfig.init_collateral_price_annualized_vol, // 1000000000000 (scaled according to zero-liquidation loan contract decimals)
